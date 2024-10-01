844c3597e9abd1b1f706ecf8cf973492ba8b7b5e
PurpleIris2 <haileyvillemuer@gmail.com>
2024-10-01 17:08:57 -0500

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2024.java
    Co-authored-by: Ryan B. Harvey <ryan.b.harvey@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2024.java
@@ -2,6 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.Robot;
 
+/* To connect to the Control Hub device via Wi-Fi:
+   - Connect Wi-Fi to the FTC-25619 network
+   - In command line, run: adb connect 192.168.43.1:5555
+   - To check that it is connected, run: adb devices -l
+   - Check that the REV Robotics Control Hub shows up above
+   - in Android Studio with a green dot (connected)
+ */
+ 
 public class Robot2024 extends Robot {
 
 }
