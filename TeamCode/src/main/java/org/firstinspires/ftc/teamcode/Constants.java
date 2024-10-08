03585c5b62e3caa59e9903fe46968804f3d9a426
PurpleIris2 <haileyvillemuer@gmail.com>
2024-10-08 17:34:08 -0500

    Added calculation for mecanum wheels in Drivetrain, strings in Constants, and base code for DriveRobot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 public final class Constants {
+    public static final class HardwareMapping{
+        public static final String frontLeftWheel = "front_left_wheel";
+        public static final String frontRightWheel = "front_right_wheel";
+        public static final String backLeftWheel = "back_left_wheel";
+        public static final String backRightWheel = "back_right_wheel";
 
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import org.firstinspires.ftc.teamcode.Constants;
+
+public class Drivetrain extends SubsystemBase {
+    private Motor frontLeftWheel;
+    private Motor frontRightWheel;
+    private Motor backLeftWheel;
+    private Motor backRightWheel;
+
+    private Telemetry mTelemetry;
+
+    public Drivetrain(HardwareMap hardwareMap, Telemetry telemetry) {
+        frontLeftWheel = new Motor(hardwareMap, Constants.HardwareMapping.frontLeftWheel);
+        frontRightWheel = new Motor(hardwareMap, Constants.HardwareMapping.frontRightWheel);
+        backLeftWheel = new Motor(hardwareMap, Constants.HardwareMapping.backLeftWheel);
+        backRightWheel = new Motor(hardwareMap, Constants.HardwareMapping.backRightWheel);
+
+        frontLeftWheel.setInverted(false);
+        frontRightWheel.setInverted(false);
+        backRightWheel.setInverted(false);
+        backLeftWheel.setInverted(false);
+
+        frontLeftWheel.setRunMode(Motor.RunMode.RawPower);
+        frontRightWheel.setRunMode(Motor.RunMode.RawPower);
+        backLeftWheel.setRunMode(Motor.RunMode.RawPower);
+        backRightWheel.setRunMode(Motor.RunMode.RawPower);
+
+
+
+        mTelemetry = telemetry;
+    }
+    public void driveRobot(double power, double theta, double turn){
+        double sinTheta = Math.sin(theta - Math.PI/4);
+        double cosTheta = Math.cos(theta - Math.PI/4);
+        double max = Math.max( Math.abs(sinTheta), Math.abs(cosTheta));
+        sinTheta = sinTheta/max;
+        cosTheta = cosTheta/max;
+
+        double leftFront = power * cosTheta + turn;
+        double rightFront = power * sinTheta - turn;
+        double leftRear = power * sinTheta + turn;
+        double rightRear = power * cosTheta - turn;
+        double scaling = power + Math.abs(turn);
+
+        if (scaling > 1){
+            leftFront /= scaling;
+            rightFront /= scaling;
+            leftRear /= scaling;
+            rightFront /= scaling;
+
+
+        }
+    }
+}
+
