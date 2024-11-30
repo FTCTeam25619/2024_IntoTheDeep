354a3dba7fda6e2e463b27b5498436bf9d9437c2
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2024-11-30 16:34:28 -0600

    Move to hue-based color matching and use NormalizedColorSensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/GamePieceColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/GamePieceColor.java
@@ -1,33 +1,23 @@
 package org.firstinspires.ftc.teamcode.lib;
 
+import org.firstinspires.ftc.teamcode.ConfigConstants;
 import org.firstinspires.ftc.teamcode.Constants;
 
 public enum GamePieceColor {
-    BLUE(0, 0, 255),
-    RED(255, 0, 0),
-    YELLOW(100, 200, 50),
-    BLACK(0, 0, 0);
+    BLUE(ConfigConstants.Colors.BLUE_HUE_MIN, ConfigConstants.Colors.BLUE_HUE_MAX),
+    RED(ConfigConstants.Colors.RED_HUE_MIN, ConfigConstants.Colors.RED_HUE_MAX),
+    YELLOW(ConfigConstants.Colors.YELLOW_HUE_MIN, ConfigConstants.Colors.YELLOW_HUE_MAX),
+    BLACK(ConfigConstants.Colors.BLACK_HUE_MIN, ConfigConstants.Colors.BLACK_HUE_MAX);
 
-    public final int red;
-    public final int green;
-    public final int blue;
-    public final double[] Lab;
+    public final float hueMin;
+    public final float hueMax;
 
-    private GamePieceColor(int red, int green, int blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-        this.Lab = ColorUtils.argbToLab(255, red, green, blue);
+    private GamePieceColor(float hueMin, float hueMax) {
+        this.hueMin = hueMin;
+        this.hueMax = hueMax;
     }
 
-    public boolean matches(int[] argb) {
-        return distance(this, argb) > Constants.Sensors.COLOR_MATCH_DISTANCE_THRESHOLD;
-    }
-
-    public static double distance(GamePieceColor color, int[] argb) {
-        double[] testColorLab = ColorUtils.argbToLab(argb[0], argb[1], argb[2], argb[3]);
-        return ColorUtils.ciede2000(
-                testColorLab[0], testColorLab[1], testColorLab[2],
-                color.Lab[0], color.Lab[1], color.Lab[2]);
+    public boolean matches(float hue) {
+        return hue >= hueMin && hue <= hueMax;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import android.graphics.Color;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.SensorColor;
 import com.arcrobotics.ftclib.hardware.SensorRevTOFDistance;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -25,8 +31,9 @@ public class Intake extends SubsystemBase{
     private ContinuousServo intakeLeft;
     private ContinuousServo intakeRight;
 
-    private SensorColor colorSensor;
+//    private SensorColor colorSensor;
     private SensorRevTOFDistance distanceSensor;
+    private NormalizedColorSensor colorSensor;
 
     private Telemetry mTelemetry;
 
@@ -69,27 +76,33 @@ public class Intake extends SubsystemBase{
                 Constants.Intake.SLIDE_MAX_ANGLE_RIGHT_DEG,
                 AngleUnit.DEGREES);
 
-        colorSensor = new SensorColor(hardwareMap, Constants.HardwareMapping.intakeColorSensor);
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, Constants.HardwareMapping.intakeColorSensor);
         distanceSensor = new SensorRevTOFDistance(hardwareMap, Constants.HardwareMapping.intakeColorSensor);
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight)colorSensor).enableLight(true);
+        }
 
         mTelemetry = telemetry;
     }
 
     @Override
     public void periodic() {
-        int[] argb = getColorSensorARGB();
-        mTelemetry.addData("Intake: L Pivot Pos", pivotLeft.getPosition());
-        mTelemetry.addData("Intake: R Pivot Pos", pivotRight.getPosition());
-        mTelemetry.addData("Intake: L Slide Pos", slideLeft.getPosition());
-        mTelemetry.addData("Intake: R Slide Pos", slideRight.getPosition());
+        NormalizedRGBA argb = getColor();
+        float[] hsv = new float[3];
+        Color.colorToHSV(argb.toColor(), hsv);
+        int[] argbArr = {(int)(argb.alpha * 255), (int)(argb.red * 255), (int)(argb.green * 255), (int)(argb.blue * 255)};
+//        mTelemetry.addData("Intake: L Pivot Pos", pivotLeft.getPosition());
+//        mTelemetry.addData("Intake: R Pivot Pos", pivotRight.getPosition());
+//        mTelemetry.addData("Intake: L Slide Pos", slideLeft.getPosition());
+//        mTelemetry.addData("Intake: R Slide Pos", slideRight.getPosition());
         mTelemetry.addData("Intake: L Cont Power", intakeLeft.getPower());
         mTelemetry.addData("Intake: R Cont Power", intakeRight.getPower());
-        mTelemetry.addData("Intake: Color Sensor", argb);
-        mTelemetry.addData("Intake: RED Match", GamePieceColor.RED.matches(argb));
-        mTelemetry.addData("Intake: BLUE Match", GamePieceColor.BLUE.matches(argb));
-        mTelemetry.addData("Intake: YELLOW Match", GamePieceColor.YELLOW.matches(argb));
-        mTelemetry.addData("Intake: BLACK Match", GamePieceColor.BLACK.matches(argb));
+        mTelemetry.addData("Intake: RED Match", GamePieceColor.RED.matches(hsv[0]));
+        mTelemetry.addData("Intake: BLUE Match", GamePieceColor.BLUE.matches(hsv[0])    );
+        mTelemetry.addData("Intake: YELLOW Match", GamePieceColor.YELLOW.matches(hsv[0]));
+        mTelemetry.addData("Intake: BLACK Match", GamePieceColor.BLACK.matches(hsv[0]));
         mTelemetry.addData("Intake: Color Sensor Dist (cm)", distanceSensor.getDistance(DistanceUnit.CM));
+        mTelemetry.addData("Intake: Color h", "%.3f", hsv[0]);
     }
 
     public void intakePiece() {
@@ -117,7 +130,7 @@ public class Intake extends SubsystemBase{
         slideRight.setPosition(position.rightPosition);
     }
 
-    private int[] getColorSensorARGB() {
-        return colorSensor.getARGB();
+    private NormalizedRGBA getColor() {
+        return colorSensor.getNormalizedColors();
     }
 }
